97b838df05b1fa204700b90e5012e44edae2b594
ogontaro <ogontaro@gmail.com>
2019-04-18 01:57:57 +0900

    add lib dir to load path

--- server/config/application.rb
+++ server/config/application.rb
@@ -33,5 +33,6 @@ module MoviePicksServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.paths.add "lib", eager_load: true
   end
 end
