bfb8e22f46e9a7a20b64b346ca8226e6b85b85a4
ogontaro <ogontaro@gmail.com>
2019-06-27 00:34:23 +0900

    add guidecategory to seed

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -7,3 +7,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Api::Repository::GuideCategoryRepository.all.result.each { |category| category.to_model }
