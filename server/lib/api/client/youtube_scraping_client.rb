4a5de8a8b79ae7071afd39e2db8630ba34d07b90
ogontaro <ogontaro@gmail.com>
2019-08-23 03:36:43 +0900

    add http header

--- server/lib/api/client/youtube_scraping_client.rb
+++ server/lib/api/client/youtube_scraping_client.rb
@@ -7,15 +7,15 @@ module Api
       end
 
       def channel_about(channel_id)
-        request_with_error_handle { Faraday.get("https://www.youtube.com/channel/#{channel_id}/about") }
+        request_with_error_handle { youtube_connection.get("/channel/#{channel_id}/about") }
       end
 
       def channel_video(channel_id)
-        request_with_error_handle { Faraday.get("https://www.youtube.com/channel/#{channel_id}/video&playnext=1&index=1") }
+        request_with_error_handle { youtube_connection.get("/channel/#{channel_id}/video&playnext=1&index=1") }
       end
 
       def search(query)
-        request_with_error_handle { Faraday.get("https://www.youtube.com/results?search_query=#{CGI.escape(query)}") }
+        request_with_error_handle { youtube_connection.get("/results?search_query=#{CGI.escape(query)}") }
       end
 
       private
@@ -23,6 +23,22 @@ module Api
           response = faraday_request.call
           raise YoutubeScrapingClientError.new(response) unless response.success?
           response
+        end
+
+        def youtube_connection
+          connection = Faraday.new("https://www.youtube.com")
+          connection.headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+            # "Accept-Encoding": "gzip, deflate, br",
+            # "Accept-Language": "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Cache-Control": "max-age=0",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+          }
+          connection
         end
     end
   end
--- server/lib/tasks/add_channel_id_by_top_viewcount.rake
+++ server/lib/tasks/add_channel_id_by_top_viewcount.rake
@@ -2,22 +2,16 @@
 
 namespace :channels do
   desc ""
-  task add_channel_by_top_viewcount: :environment do
+  task add_channel_id_by_top_viewcount: :environment do
     STDOUT.sync = true
 
-    channels = try_api_with_wait { Api::Repository::ChannelSearchRepository.all(order: "viewcount") }
+    channels = try_api_with_wait { Api::Repository::ChannelSearchRepository.all(part: "id", order: "viewcount") }
 
     loop do
       channels.result.each do |c|
         Raven.extra_context(c: c.to_hash)
-        channel_entity = Api::Repository::YoutubeScraping::ChannelRepository.find(c.to_model.channel_id).result
-        next if channel_entity.blank?
-
-        Raven.extra_context(channel: channel_entity.to_hash)
-        channel = channel_entity.to_model
-        channel.save
-        puts "title: #{channel.title}, id:#{channel.channel_id} created"
-        sleep 2
+        channel = c.to_model
+        puts "channel_id:#{channel.channel_id} created"
       end
       channels = try_api_with_wait { channels.next }
     end
